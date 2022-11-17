71fdea2a9fbd805605af7bf07d83ed5580e9a775
Dima Kobzar <kobzar7dima@gmail.com>
2022-11-17 23:25:47 +0200

    disabled all poi markers on the map

--- src/modules/map.js
+++ src/modules/map.js
@@ -9,32 +9,12 @@ export function initMap() {
 
   let styles = [
     {
-      featureType: "poi.business",
-      elementType: "labels",
-      stylers: [{ visibility: "off" }],
-    },
-    {
       featureType: "administrative",
       elementType: "labels",
       stylers: [{ visibility: "off" }],
     },
     {
-      featureType: "poi.medical",
-      elementType: "labels",
-      stylers: [{ visibility: "off" }],
-    },
-    {
-      featureType: "poi.school",
-      elementType: "labels",
-      stylers: [{ visibility: "off" }],
-    },
-    {
-      featureType: "poi.sports_complex",
-      elementType: "labels",
-      stylers: [{ visibility: "off" }],
-    },
-    {
-      featureType: "poi.park",
+      featureType: "poi",
       elementType: "labels",
       stylers: [{ visibility: "off" }],
     },
